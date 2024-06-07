a704b4789f689d26911c8a19962f0a7c17ead0d4
khushalGrover <khushal15grover@gmail.com>
2024-06-07 16:27:49 +0530

    unknown changes

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'r3f-vite-starter',
+  base: 'R3f-Scroll-Animation',
   plugins: [react()],
   server: {
     host: true
