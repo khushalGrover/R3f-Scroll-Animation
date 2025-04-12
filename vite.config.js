79aa3b27f10a110b960a7049eff774cca5c1e2c8
khushalGrover <khushal15grover@gmail.com>
2025-04-12 23:35:12 +0530

    minor deplpoy testing 2

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'R3F-Scroll-Animation',
+  base: 'R3f-Scroll-Animation',
   plugins: [react()],
   server: {
     host: true
